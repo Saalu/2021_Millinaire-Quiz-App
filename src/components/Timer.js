be86d6f36f5100aff66e570ee123fe2f3379c97b
Saalu <saaluissak@gmail.com>
2021-06-11 11:47:07 +0000

    UI setup and partial logic

---
+++ src/components/Timer.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+function Timer({ setStop, questionNumber }) {
+  const [timer, setTimer] = useState(3);
+
+  useEffect(() => {
+    if (timer === 0) return setStop(true);
+    const interval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [setStop, timer]);
+
+  useEffect(() => {
+    setTimer(3);
+  }, [questionNumber]);
+
+  //   main Component
+  return timer;
+}
+
+export default Timer;
